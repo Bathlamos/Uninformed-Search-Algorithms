2e6155c5b0568280247df2d3c67477b53f934b4f
Bathlamos <plegault@live.ca>
2015-02-10 17:45:31 -0500

    DFS is now iterative

--- src/main/java/cc/legault/csi4106/a1/searchAlgorithms/DFS.java
+++ src/main/java/cc/legault/csi4106/a1/searchAlgorithms/DFS.java
@@ -1,67 +1,76 @@
+/**
+ * Philippe Legault - 6376254
+ *
+ * CSI 4106 - Artificial Intelligence I
+ * University of Ottawa
+ * February 2015
+ */
+
 package cc.legault.csi4106.a1.searchAlgorithms;
 
+import com.google.common.collect.Sets;
 import org.jgrapht.alg.NeighborIndex;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.SimpleWeightedGraph;
 
 import java.util.*;
 
+/**
+ * Executes a Depth-First Search on a given graph.
+ * @param <V> The type of the vertices.
+ */
 public class DFS<V> implements UninformedSearchAlgorithm<V> {
 
     private int maxNumberOfNodesInMemory = 0;
     private int totalNumberOfNodesGenerated = 0;
 
+    /**
+     * @inheritDoc
+     */
     @Override
     public <E extends DefaultWeightedEdge> List<E> findPath(SimpleWeightedGraph<V, E> graph, V origin, V destination) {
-
-        //Used to traverse the tree in DFS
-        Stack<V> stack = new Stack<>();
-        stack.add(origin);
-
-        //Keep tracks of the visited nodes and their direct ancestor
-        Map<V, V> parent = new HashMap<>();
-        parent.put(origin, null);
-
-        maxNumberOfNodesInMemory = 1;
-        totalNumberOfNodesGenerated = 1;
-
-        while(!stack.isEmpty()){
-
-            //Extract the head of the queue
-            V currentNode = stack.pop();
-
-            //Compare the head of the queue with the destination
-            if(currentNode.equals(destination)){
-                //We've found a path. Build and return it.
-                List<E> edges = new LinkedList<>();
-                while(!destination.equals(origin)){
-                    V ancestor = parent.get(destination);
-                    edges.add(0, graph.getEdge(destination, ancestor));
-                    destination = ancestor;
-                }
-                return edges;
-            }
-
-            //Add the adjacent vertices to the queue,
-            // if they haven't been visited yet
-            NeighborIndex<V, E> neighbourIndex = new NeighborIndex<>(graph);
-            for(V adjacentVertex: neighbourIndex.neighborsOf(currentNode))
-                if(!parent.containsKey(adjacentVertex)){
-                    parent.put(adjacentVertex, currentNode);
-                    stack.push(adjacentVertex);
-                    totalNumberOfNodesGenerated++;
-                }
-
-            maxNumberOfNodesInMemory = Math.max(maxNumberOfNodesInMemory, stack.size());
-        }
-
-        throw new RuntimeException("No path exist from the origin to the destination");
+        maxNumberOfNodesInMemory = 0;
+        totalNumberOfNodesGenerated = 0;
+        List<E> edges = findPath(graph, origin, destination, Sets.newHashSet(origin), 0);
+        if(edges == null)
+            throw new RuntimeException("No path exist from the origin to the destination");
+        return edges;
     }
 
+    private <E extends DefaultWeightedEdge> List<E> findPath(SimpleWeightedGraph<V, E> graph, V origin, V destination, Set<V> visited, int numNodesGenerated) {
+
+        if(destination == origin)
+            return new LinkedList<E>();
+
+        //Add the adjacent vertices to the queue,
+        // if they haven't been visited yet
+        NeighborIndex<V, E> neighbourIndex = new NeighborIndex<>(graph);
+        for(V adjacentVertex: neighbourIndex.neighborsOf(origin))
+            if(!visited.contains(adjacentVertex)){
+                visited.add(adjacentVertex);
+                totalNumberOfNodesGenerated++;
+                maxNumberOfNodesInMemory = Math.max(maxNumberOfNodesInMemory, numNodesGenerated + 1);
+                List<E> edges = findPath(graph, adjacentVertex, destination, visited, numNodesGenerated + 1);
+                if(edges != null){
+                    //We've found the destination.
+                    edges.add(0, graph.getEdge(origin, adjacentVertex));
+                    return edges;
+                }
+            }
+
+        return null;
+    }
+
+    /**
+     * @inheritDoc
+     */
     public int getMaximumNodesInMemory(){
         return maxNumberOfNodesInMemory;
     }
 
+    /**
+     * @inheritDoc
+     */
     public int getTotalNumberOfNodesGenerated(){
         return totalNumberOfNodesGenerated;
     }
